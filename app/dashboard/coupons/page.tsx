7c1239b88e1e2b63555061d84f5ba2ad36e93661
saifactiify <sai@factiify.com>
2025-01-22 16:45:38 +0530

    updated coupon page.tsx file

--- app/dashboard/coupons/page.tsx
+++ app/dashboard/coupons/page.tsx
@@ -1,38 +1,123 @@
 "use client";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Coupon,
+  couponResponse,
+  MasterDataItem,
+} from "@/app/ui/models/response";
+import Image from "next/image";
+import EditIcon from "@/public/edit-icon.svg";
+import DeleteIcon from "@/public/delete-icon.svg";
+import AddNewCouponForm from "@/app/ui/component/popup_model/addNewCouponForm";
+import UpdateExistingCouponForm from "@/app/ui/component/popup_model/updateExistingCouponForm";
 import Swal from "sweetalert2";
-import { couponResponse, masterDataResponse } from "../../ui/models/response";
-
+import { usePathname } from "next/navigation";
+import { camelCaseToTitle } from "@/app/ui/utility/textToCamelConverter";
 function page() {
-  const route = useRouter();
+  const [couponCompleteList, setCouponcCompleteList] = useState<Coupon[]>([]);
+  const [couponList, setCouponList] = useState<Coupon[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  const [editData, setEditData] = useState<Coupon>();
 
-  const handleSubmit = () => {
-    fetchMasterData();
-    route.push("/dashboard");
+  const [categoryOption, setCategoryOption] = useState<MasterDataItem[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const currentpath=usePathname();
+
+  const updatedCurrentPath = currentpath
+  .split("/") 
+  .filter(Boolean) 
+  .map((segment) => camelCaseToTitle(segment)) 
+  .join(" / "); 
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("masterData");
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      const categorys: MasterDataItem[] = parsedData?.couponCategory || [];
+      setCategoryOption(categorys);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (selectedCategory === "All") {
+      setCouponList(couponCompleteList);
+    } else {
+      const updatedList = couponCompleteList.filter(
+        (item) => item.category === selectedCategory
+      );
+      setCouponList(updatedList);
+    }
+  }, [selectedCategory, couponCompleteList]);
+
+  useEffect(() => {
+    FetchAPI();
+  }, []);
+  useEffect(() => {
+    console.log("couponList:", couponList);
+  }, [couponList]);
+
+  const deleteAPI = async (id: string) => {
+    try {
+      const response = await fetch("/api/coupon/delete", {
+        method: "DELETE",
+        headers: {
+          // authorization: Bearer ${localStorage.getItem('authToken')},
+          id: id,
+        },
+        // body: JSON.stringify({
+        //   is_completed: true, // Send the updated content to the server
+        // }),
+      });
+      const sResponse: couponResponse =
+        (await response.json()) as couponResponse;
+
+      console.log("response:", sResponse);
+      if (sResponse.status) {
+        setCouponList((prev) => {
+          const updatedList = prev.filter((item) => item._id !== id);
+          return updatedList;
+        });
+        Swal.fire({
+          title: "Delete Coupon Successful",
+          icon: "success",
+        });
+      } else {
+        Swal.fire({
+          title: "Failed to delete Coupon",
+          text: sResponse.message || "An unknown error occurred.",
+          icon: "error",
+        });
+      }
+    } catch (error) {
+      console.error("Error updating notes:", error);
+      Swal.fire({
+        title: "Error",
+        text: "An error occurred while delete the coupon. Please try again.",
+        icon: "error",
+      });
+    }
   };
 
-  const fetchMasterData = async () => {
+  const FetchAPI = async () => {
     try {
-      const response = await fetch("/api/masterdata/get", {
+      const response = await fetch("/api/coupon/get", {
         method: "GET",
         // headers: {
         //   authorization: Bearer ${localStorage.getItem('authToken')},
         //   'notesId': String(currentCallNote?.id),
         // },
+        // body: JSON.stringify({
+        //   is_completed: true, // Send the updated content to the server
+        // }),
       });
-      const sResponse: masterDataResponse =
-        (await response.json()) as masterDataResponse;
+      const sResponse: couponResponse =
+        (await response.json()) as couponResponse;
 
-      //   console.log("response:", sResponse);
+      console.log("response:", sResponse);
       if (sResponse.status) {
-        localStorage.setItem("masterData", JSON.stringify(sResponse.data));
-      } else {
-        Swal.fire({
-          title: "Error",
-          text: sResponse.message,
-          icon: "error",
-        });
+        setCouponList(sResponse.data);
+        setCouponcCompleteList(sResponse.data);
       }
     } catch (error) {
       console.error("Error updating notes:", error);
@@ -43,14 +128,180 @@ function page() {
       });
     }
   };
+
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <button
-        className="p-5 px-20 bg-blue-500 text-white text-xl "
-        onClick={() => handleSubmit()}
-      >
-        Login
-      </button>
+    <div className="w-full h-screen p-4 bg-gray-100">
+      <div className="w-full h-14 mb-4 flex justify-between items-center">
+        <div className="text-2xl font-bold">
+          {updatedCurrentPath}
+        </div>
+        <button
+          className="p-2 bg-blue-600 rounded text-white mr-3 font-medium"
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        >
+          Add New Coupon
+        </button>
+      </div>
+      <div className="flex p-4 border-[#eaeaea] bg-white gap-4 ml-[-12px]">
+        {/* Add "All Categories" button */}
+        <button
+          className={`px-6 py-2 border rounded-lg text-sm font-semibold ${
+            selectedCategory === "All"
+              ? "bg-orange-500 text-white"
+              : "text-black border-[#748892]"
+          }`}
+          onClick={() => setSelectedCategory("All")}
+        >
+          All 
+        </button>
+
+        {/* Map through category options */}
+        {categoryOption.map((status) => (
+          <button
+            key={status.id}
+            className={`px-2 py-1 border rounded-lg text-sm font-semibold ${
+              selectedCategory === status.name
+                ? "bg-orange-500 text-white"
+                : "text-black border-[#748892]"
+            }`}
+            onClick={() => setSelectedCategory(status.name)}
+          >
+            {status.name}
+          </button>
+        ))}
+      </div>
+
+      <div className="overflow-auto rounded-lg shadow-lg bg-white">
+        <table className="min-w-full border-collapse border border-gray-200">
+          <thead>
+            <tr className="bg-gray-800 text-white">
+              <th className="p-3 text-left">S.No</th>
+              <th className="p-3 text-left">Image</th>
+              <th className="p-3 text-left">Name</th>
+              <th className="p-3 text-left">Coupon Code</th>
+              <th className="p-3 text-left">Description</th>
+              <th className="p-3 text-left">Discount Type</th>
+              <th className="p-3 text-left">Type of Booking</th>
+              <th className="p-3 text-left">Category</th>
+              <th className="p-3 text-left">Edit</th>
+              <th className="p-3 text-left">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {couponList.length > 0 ? (
+              couponList.map((coupon, index) => (
+                <tr key={coupon.code} className="even:bg-gray-100">
+                  <td className="p-3 border border-gray-200 text-center">
+                    {index + 1}
+                  </td>
+                  <td className="p-3 border border-gray-200">
+                    <img
+                      src={coupon.image}
+                      alt={coupon.name}
+                      className="w-16 h-16 object-contain rounded"
+                    />
+                  </td>
+                  <td className="p-3 border border-gray-200">{coupon.name}</td>
+                  <td className="p-3 border border-gray-200">{coupon.code}</td>
+                  <td className="p-3 border border-gray-200">
+                    {coupon.description}
+                  </td>
+                  <td className="p-3 border border-gray-200">
+                    {coupon.discountType}
+                  </td>
+                  <td className="p-3 border border-gray-200">
+                    {coupon.typeOfBooking.split(",").join(", ")}
+                  </td>
+                  <td className="p-3 border border-gray-200">
+                    {coupon.category}
+                  </td>
+                  <td className="p-3 border border-gray-200 text-center cursor-pointer">
+                    <div
+                      className="flex justify-center items-center space-x-2"
+                      onClick={() => {
+                        setIsEditOpen(true);
+                        setEditData(coupon);
+                      }}
+                    >
+                      <Image
+                        src={EditIcon}
+                        alt="edit icon"
+                        width={20}
+                        height={20}
+                      />
+                      {/* <span className="text-blue-600 font-semibold">Edit</span> */}
+                    </div>
+                  </td>
+                  <td
+                    className="p-3 border border-gray-200 text-center cursor-pointer"
+                    onClick={() => {
+                      deleteAPI(coupon._id);
+                    }}
+                  >
+                    <div className="flex justify-center items-center space-x-2">
+                      <Image
+                        src={DeleteIcon}
+                        alt="delete icon"
+                        width={20}
+                        height={20}
+                      />
+                      {/* <span className="text-red-600 font-semibold">Delete</span> */}
+                    </div>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td
+                  colSpan={10}
+                  className="p-3 text-center text-gray-500 border border-gray-200"
+                >
+                  No coupons available.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+      {isOpen && (
+        <AddNewCouponForm
+          setIsOpen={setIsOpen}
+          onSave={(data) => {
+            if (typeof data === "function") {
+              setCouponList(data);
+            } else {
+              setCouponList((prev) => [...prev, data]);
+              setCouponcCompleteList((prev)=>[...prev,data]);
+            }
+          }}
+        />
+      )}
+      {isEditOpen && (
+        <UpdateExistingCouponForm
+          setIsOpen={setIsEditOpen}
+          onSave={(updatedCoupon) => {
+            setCouponList((prevCoupons) =>
+              prevCoupons.map(
+                (coupon) =>
+                  coupon._id === updatedCoupon._id
+                    ? updatedCoupon // Replace the matching coupon
+                    : coupon // Keep the others unchanged
+              )
+            );
+            setCouponcCompleteList((prevCoupons) =>
+              prevCoupons.map(
+                (coupon) =>
+                  coupon._id === updatedCoupon._id
+                    ? updatedCoupon // Replace the matching coupon
+                    : coupon // Keep the others unchanged
+              )
+            );
+          }}
+          data={editData}
+        />
+      )}
     </div>
   );
 }
